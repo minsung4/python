769e486a8ac57c431e55ac1fb92cdfd54e4947ba
Minsung Kim <billyco@gmail.com>
2018-09-28 23:21:33 +0900

    add notbook

--- ex_1.py
+++ ex_1.py
@@ -1,10 +1,10 @@
 import FinanceDataReader as fdr 
 import numpy as np 
-import matplotlib 
+import matplotlib.pyplot as plt 
 import pandas as pd 
 
 
 
 df = fdr.DataReader('138040', '2012-01-01') #메리츠금융지주#
 
-print(df)
+df['Open'].plot()
